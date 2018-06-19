62c458275e25a96b19b9ccd9875786c0d7560e34
Adam Jermstad <adam@townsquaredigital.com>
2018-06-18 21:24:10 -0500

    lesson 169

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './containers/App';
+import { searchRobots } from './reducers';
 import './index.css';
 import 'tachyons'
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(searchRobots);
+
 ReactDOM.render(
-    <App />, document.getElementById('root'));
+    <Provider store={store}>
+        <App store={store} />    
+    </Provider>
+,document.getElementById('root'));
 registerServiceWorker();
